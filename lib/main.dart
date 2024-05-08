d239cf93bc7db99fb8acb4bf79f0abbb63abab0c
rsgamertv <Wersuser2@gmail.com>
2024-05-08 13:06:36 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
               '/MainPage':(context) => MainPage(),
               '/SecondPage':(context) => SecondPage()
             },
-            initialRoute: '/SecondPage',
+            initialRoute: '/MainPage',
         );
       },
     );
